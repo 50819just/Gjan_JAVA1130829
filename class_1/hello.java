2a1c47faa29ac852736e25951bb69e09a32501e3
unknown <justin50819just@gmail.com>
2024-08-29 21:48:11 +0800

    first commit-2

---
+++ class_1/hello.java
@@ -0,0 +1,10 @@
+package class_1;
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+// java程式 進入點宣告
+//大寫字 系統程式
+//螢幕輸出 System.out.println("hello");
+//螢幕輸入 System.in.println("hello");
---
+++ class_1/helloworld.java
@@ -0,0 +1,10 @@
+package class_1;
+public class helloworld {
+    public static void main(String[] args) {
+        
+    }  
+    }
+    // java程式 進入點宣告
+    //大寫字 系統程式
+    //螢幕輸出 System.out.println("hello");
+    //螢幕輸入 System.in.println("hello");

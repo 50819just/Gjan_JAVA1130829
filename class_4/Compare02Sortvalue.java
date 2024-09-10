26082d0b7da94ec25efc883e9bbae81bedb76b82
unknown <justin50819just@gmail.com>
2024-09-10 21:46:25 +0800

    112.0910commit-2

--- class_4/Compare02Sortvalue.java
+++ class_4/Compare02Sortvalue.java
@@ -1,21 +1,26 @@
-// package class_4;
+package class_4;
 
-// public class Compare02Sortvalue {
-//     public static void main(String[] args) 
-//       { if(v1>v2){
-//         int temp =v1;
-//         v1=v2;
-//         v2=temp;
-//        }
-//         if(v1>v3){
-//             int temp=v1;
-//             v1=v3;
-//             v3=temp;
-//         }
-//         if(v2>v3){
-//             int temp=v2;
-//             v2=v3;
-//             v3=temp
-//         }
-//     }
-// }
+public class Compare02Sortvalue {
+    public static void main(String[] args) 
+
+      {   int v1 = (int) (Math.random() * 10) + 1;
+        int v2 = (int) (Math.random() * 10) + 1;
+        int v3 = (int) (Math.random() * 10) + 1;
+        System.out.println("v1=" + v1 + "v2=" + v2 + "v3=" + v3);
+        if(v1>v2){
+        int temp =v1;
+        v1=v2;
+        v2=temp;
+       }
+        if(v1>v3){
+            int temp=v1;
+            v1=v3;
+            v3=temp;
+        }
+        if(v2>v3){
+            int temp=v2;
+            v2=v3;
+            v3=temp ;
+        }
+    }
+}

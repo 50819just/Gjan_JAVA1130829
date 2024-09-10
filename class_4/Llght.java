443c75cdee11de96e376a8d187a9dcb3dc319630
unknown <justin50819just@gmail.com>
2024-09-10 21:40:59 +0800

    112.0910commit-1

---
+++ class_4/Llght.java
@@ -0,0 +1,18 @@
+package class_4;
+
+public class Llght {
+  public static void main(String[] args) {
+  int x =(int)(Math.random()*5)+1;
+  switch (x) {
+      case 1:
+      System.out.println((x+"紅燈"));
+      break;
+      case 2 ;
+      System.out.println((x+"黃燈"));
+      break;
+      default:
+      System.out.println((x+"綠燈"));
+      break;
+  }
+}
+}
